ba43219f64b12c7705fc326f42b35103fc500f9c
MortezaVeismordi <mveismordi@gmail.com>
2025-02-16 18:18:09 +0330

    more changes to the blog single and home

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post , Category
 
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     empty_value_display = '-empty-'
-    list_display = ('title' , 'counted_views' , 'published_date' , 'created_at' , 'updated_time')
-    list_filter = ('status' ,)
+    list_display = ('title' ,'author' ,'counted_views' , 'published_date' , 'created_at' , 'updated_time')
+    list_filter = ('status' , 'author')
     #ordering = ['created_at']
     search_fields = ['title' , 'content']
+
+
+admin.site.register(Category)
 admin.site.register(Post , PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,17 @@ def blog_view(request):
     return render(request , "blog/blog-home.html" , context)
 
 def blog_single (request , pid):
-    post = get_object_or_404(Post , pk=pid)
+    now = timezone.now()
+    post = get_object_or_404(Post , pk=pid , status = 1)
     post.counted_views += 1
     post.save()
-    context = {'post':post }
+    previous_post = Post.objects.filter(published_date__lt=post.published_date , published_date__lte=now).order_by('-published_date').first()
+    next_post = Post.objects.filter(published_date__gt=post.published_date , published_date__lte=now).order_by('published_date').first()
+    context = {
+        'post': post,
+        'previous_post': previous_post,
+        'next_post': next_post,
+    }
     return render(request , 'blog/blog-single.html' , context)
 
 
