e7ec413afd524eed0e6a1cc03c33230e934df4e7
MortezaVeismordi <mveismordi@gmail.com>
2025-02-18 02:08:52 +0330

    fsl 8 1 vid left

---
+++ website/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from website.models import Contact
+
+#-----------------------------------------------
+
+
+class NameForm(forms.Form):
+    name = forms.CharField(max_length=255)
+
+
+#-----------------------------------------------
+
+
+class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        
--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from website.models import Contact
+from website.forms import NameForm , ContactForm
+
+#---------------------------------------------------------------------
 
 def index(request):
     return render(request , 'website/index.html')
@@ -8,7 +12,16 @@ def about_me(request):
     return render(request , 'website/about.html')
 
 def contact_me(request):
-    return render(request , 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm
+    return render(request , 'website/contact.html' , {'form':form})
 
 def test_view(request):
+    if request.method =='POST':
+        name = request.POST.get("name")
+        form = NameForm(request.POST)
+        
     return render(request , 'test.html' , {'name': 'Morteza'})
